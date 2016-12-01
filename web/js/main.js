b10226aecef4ddc967b8507404cf9feeb0e70323
Vladimir Bespechny <yankeeeeeman@gmail.com>
2016-12-01 11:18:40 +0200

    [+] scalingImg

--- web/js/main.js
+++ web/js/main.js
@@ -4,32 +4,54 @@ window.onload  = function () {
 
 
 let AppJS = {
+    params: {
+        imgW: 100,
+        imgH: 100,
+        imgL: 0,
+        imgT: 0,
+    },
+
     init: function () {
         AppJS.ready();
     },
 
     ready: function () {
         AppJS.renderCanvas();
-        AppJS.scaling();
-        let photoBorder = document.getElementById('photoBorder');
-        photoBorder.onmousedown = function (e) {
-            AppJS.startMoving(e, this);
-        }
+        AppJS.handlers();
     },
 
-    scaling: function () {
+    handlers: function () {
+        let photoBorder     = document.getElementById('photoBorder');
+        let plus            = document.querySelector('.js-plus');
+        let minus           = document.querySelector('.js-minus');
+        photoBorder.onmousedown     = function (e){ AppJS.startMoving(e, this); };
+        plus.onclick                = function () { AppJS.scaling('plus'); };
+        minus.onclick               = function () { AppJS.scaling('minus'); };
+    },
 
+    scaling: function (sign) {
+        if (sign === 'plus') {
+            AppJS.params.imgW += 10;
+            AppJS.params.imgH += 10;
+        } else if (sign === 'minus') {
+            AppJS.params.imgW -= 10;
+            AppJS.params.imgH -= 10;
+        }
+        AppJS.renderCanvas();
     },
 
     startMoving: function (e, border) {
-        console.log('Start');
+        let dragStartObj = {
+            x: e.offsetX,
+            y: e.offsetY,
+            imgL: AppJS.params.imgL,
+            imgT: AppJS.params.imgT,
+        };
         border.onmousemove = function (e) {
-            AppJS.moving(e);
-            console.log('Moving');
+            AppJS.moving(e, dragStartObj);
         };
         border.onmouseup = function (e) {
             AppJS.endMoving(e, border);
-            console.log('End');
         };
     },
 
@@ -37,23 +59,32 @@ let AppJS = {
         border.onmousemove = null;
     },
 
-    moving: function (e) {
+    moving: function (e, start) {
+        let startL = start.x;
+        let startT = start.y;
+        let imgStartL = start.imgL;
+        let imgStartT = start.imgT;
+
         let left = e.offsetX;
         let top  = e.offsetY;
-        console.log(e, left, top);
 
-        AppJS.renderCanvas(left, top);
+        let translateX = left - startL;
+        let translateY = top  - startT;
+
+        AppJS.params.imgL = imgStartL + translateX;
+        AppJS.params.imgT = imgStartT + translateY;
+        AppJS.renderCanvas();
     },
 
-    renderCanvas: function (left, top) {
+    renderCanvas: function () {
         let photoBorder = document.getElementById('photoBorder');
         let ctx = photoBorder.getContext('2d');
 
-        let rectH = 100;
-        let rectW = 100;
+        let rectW = AppJS.params.imgW;
+        let rectH = AppJS.params.imgH;
 
-        let rectL = left ? left : photoBorder.width  / 2;
-        let rectT = top  ? top  : photoBorder.height / 2;
+        let rectL = AppJS.params.imgL;
+        let rectT = AppJS.params.imgT;
 
         ctx.clearRect(0, 0, photoBorder.width, photoBorder.height);
 
